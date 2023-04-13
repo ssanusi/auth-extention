b15846b653a95425f961de0b2555975b1e975744
Sulaiman Sanusi <sulaiman@okra.ng>
2023-04-13 02:11:52 +0100

    added authentication

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
-
+import { Auth } from './decorators/auth.decorators';
+import { AuthType } from './enums/auth-type.enum';
+@Auth(AuthType.None)
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
@@ -14,7 +23,17 @@ export class AuthenticationController {
 
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
-  signIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto);
+  async signIn(
+    @Body() signInDto: SignInDto,
+    @Res({ passthrough: true }) response: Response & { cookie: any },
+  ) {
+    const accessToken = await this.authService.signIn(signInDto);
+    response.cookie('access_token', accessToken, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: true,
+      maxAge: 60 * 60 * 24 * 30,
+    });
+    return accessToken ;
   }
 }
---
+++ src/iam/authentication/guards/access-token/access-token.guard.ts
@@ -0,0 +1,51 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import jwtConfig from 'src/iam/config/jwt.config';
+import { REQUEST_USER_KEY } from 'src/iam/iam.constant';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromRequest(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const decoded = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
+      console.log({ decoded });
+      request[REQUEST_USER_KEY] = decoded;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromRequest(request: Request): string | undefined {
+    const authorizationHeader = request.headers.authorization;
+    if (!authorizationHeader) {
+      return undefined;
+    }
+    const [bearer, token] = authorizationHeader.split(' ');
+    if (bearer !== 'Bearer' || !token) {
+      return undefined;
+    }
+    return token;
+  }
+}
